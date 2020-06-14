548c93861dd5c747e868e1db2c2c24203a331f0b
Anas <hassanali2069@gmail.com>
2020-06-14 18:03:11 +0500

    https://github.com/AnasAamir/js-work.git

---
+++ app.js
@@ -0,0 +1,27 @@
+// Chapter 1 Task 1
+// alert("Welcome to my site");
+// Chapter 1 Task 2
+// alert("Error ! Please enter a valid password");
+// Chapter 1 Task 3
+// alert("Welcome to JS Land\nHappy Coding ");
+// Chapter 1 Task 4
+// alert("Welcome to JS Land");
+// alert("Happy coding");
+// Chapter 1 Task 5
+// Done
+// Chapter 1 Task 6
+// Done
+// Chapter 1 Task 7
+// Done
+
+
+
+// Chapter 2 Task 1
+var username = 5;
+// Chapter 2 Task 2
+var myname = "myname";
+
+// Chapter 2 Task 3
+var z = x + y;
+document.getElementById("demo").innerHTML =
+    "The value of z is: " + z;
